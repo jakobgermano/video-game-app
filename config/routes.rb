a6ed7f3d118d6d47fd15307c16e088c3a8e576b2
jakob <jakob.germano@gmail.com>
2022-03-03 19:24:53 -0700

    migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :vehicles
+  resources :drivers
+  resources :reviews 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
