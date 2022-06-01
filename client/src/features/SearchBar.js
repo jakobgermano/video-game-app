9019362739ad41d947ce865477b35fc46c40d9b1
jakob <jakob.germano@gmail.com>
2022-05-31 20:12:26 -0600

    com

--- client/src/features/SearchBar.js
+++ client/src/features/SearchBar.js
@@ -3,14 +3,17 @@ import React, {useState, useEffect} from 'react';
     function SearchBar({games}) {
         const [searchInput, setSearchInput] = useState("")
         const [submittedSearch, setSubmittedSearch] = useState("")
+
         const handleSearch = (e) => {
             e.preventDefault()
             setSubmittedSearch(searchInput)
         }
+        
         const filteredData = games?.filter(game => game.name.toLowerCase().includes(submittedSearch.toLowerCase())).map(g => {
             if (submittedSearch.length > 0){return <div key={g.id}>{g.name}</div>}
             }
         )
+
         return(
             <div className="search">
                 <form >
